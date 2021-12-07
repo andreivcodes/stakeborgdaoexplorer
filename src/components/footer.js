c06365931e584588d473b51fa3d4a55a555ff631
Andrei <andrei@andreiv.com>
2021-12-08 01:00:53 +0200

    github link

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,9 +9,11 @@ import {
   Grid,
   GridItem,
   Text,
+  Flex,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import GitInfo from "react-git-info/macro";
+import githublogo from "./../github-logo.png";
 
 function Footer() {
   const gitInfo = GitInfo();
@@ -26,12 +28,24 @@ function Footer() {
       px={{ base: "4", md: "8" }}
       minWidth="100vw"
     >
-      <Grid templateRows="repeat(2,auto)" templateColumns="repeat(3,auto)">
+      <Grid templateRows="repeat(2,auto)" templateColumns="10vw auto 10vw">
         <GridItem row="1" colSpan="3">
           <Text fontSize="sm">***REMOVED***</Text>
         </GridItem>
         <GridItem row="2" column="1" alignSelf="end" justifySelf="start">
-          <Text fontSize="sm">build : {gitInfo.commit.hash.slice(-6)}</Text>
+          <Flex>
+            <a href="https://github.com/andreivdev/stakeborgdaoexplorer">
+              <Image
+                objectFit="contain"
+                boxSize="20px"
+                minW="20px"
+                minH="20px"
+                mr="2"
+                src={githublogo}
+              ></Image>
+            </a>
+            <Text fontSize="sm">build : {gitInfo.commit.hash.slice(-6)}</Text>
+          </Flex>
         </GridItem>
         <GridItem row="2" column="2">
           <Stat>
